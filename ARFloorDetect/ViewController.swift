1b5753d528d1e461c625c2eb8c1c482009ad31f9
DaddyKrauze <anton53429@gmail.com>
2020-07-02 16:00:07 +0300

    Initial Commit

---
+++ ARFloorDetect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARFloorDetect
+//
+//  Created by Tony Krauze on 02.07.2020.
+//  Copyright © 2020 Tony Krauze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
